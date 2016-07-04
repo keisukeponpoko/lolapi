c27565ed5d49cb9ba14fe3e9624a3792af819fc1
keisukeponpoko <k.suke.jp1990@gmail.com>
2016-07-04 23:42:50 +0900

    test

--- config/app.php
+++ config/app.php
@@ -156,8 +156,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
-        Barryvdh\Debugbar\ServiceProvider::class,
+        //Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        //Barryvdh\Debugbar\ServiceProvider::class,
 
         Ixudra\Curl\CurlServiceProvider::class,
     ],
